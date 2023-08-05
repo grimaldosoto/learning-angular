7d805df636533cdfe6c46f581c3e6be9484afcb2
grimaldo.soto <Grimaldo.SOTO@icloud.com>
2023-08-05 11:55:05 -0500

    Página 404

--- udemy-fernando-herrera.com-angular-adv/03-adminpro/src/app/pages/no-page-found/no-page-found.component.ts
+++ udemy-fernando-herrera.com-angular-adv/03-adminpro/src/app/pages/no-page-found/no-page-found.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-no-page-found',
   templateUrl: './no-page-found.component.html',
-  styles: [
-  ]
+  styleUrls: ['./no-page-found.component.css'],
 })
 export class NoPageFoundComponent {
+  year = new Date().getFullYear();
 
 }
