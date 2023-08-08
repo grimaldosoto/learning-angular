dceed97c6c50e6088dc51a0d82b4897ceda00027
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-08-07 20:53:46 -0500

    Resolucón de la tarea práctica #3 - Creación de un módulo personalizado

--- udemy-fernando-herrera.com-angular-adv/03-adminpro/src/app/app.module.ts
+++ udemy-fernando-herrera.com-angular-adv/03-adminpro/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
 
 import { AppComponent } from './app.component';
-import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
 import { AuthModule } from './auth/auth.module';
 
 
