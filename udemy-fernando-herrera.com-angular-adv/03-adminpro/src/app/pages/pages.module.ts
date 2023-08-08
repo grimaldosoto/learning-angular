bde01f5f0298f4c9bd5828da3672b5c44e092cc4
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-08-07 21:43:40 -0500

    Rutas hijas - ForChild()

--- udemy-fernando-herrera.com-angular-adv/03-adminpro/src/app/pages/pages.module.ts
+++ udemy-fernando-herrera.com-angular-adv/03-adminpro/src/app/pages/pages.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
-import { PagesComponent } from './pages.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
-
+import { PagesComponent } from './pages.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +20,10 @@ import { RouterModule } from '@angular/router';
     Grafica1Component,
     PagesComponent,
   ],
-  imports: [CommonModule, SharedModule, RouterModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule,
+  ],
 })
 export class PagesModule {}
