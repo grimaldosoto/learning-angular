5cfe338731bf186eb44393663461aaa23119cbd1
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-08-20 14:59:38 -0500

    arrobaInput - Componente incrementador

--- udemy-fernando-herrera.com-angular-adv/03-adminpro/src/app/components/incrementador/incrementador.component.ts
+++ udemy-fernando-herrera.com-angular-adv/03-adminpro/src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -7,11 +7,9 @@ import { Component } from '@angular/core';
   ]
 })
 export class IncrementadorComponent {
-  progreso: number = 50;
+  // @Input('valor') progreso: number = 50;
+  @Input() progreso: number = 50;
 
-  get gerPorcentaje(){
-    return `${this.progreso}%`;
-  }
 
   cambiarValor(valor:number){
     if(this.progreso >= 100 && valor >= 0){
