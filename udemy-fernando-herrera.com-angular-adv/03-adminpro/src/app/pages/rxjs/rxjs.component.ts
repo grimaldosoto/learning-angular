2b611dd2a6a4db1125e64bdecec32c3ffeb77b8f
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-08-29 20:47:57 -0500

    crear un observable manualmente

--- udemy-fernando-herrera.com-angular-adv/03-adminpro/src/app/pages/rxjs/rxjs.component.ts
+++ udemy-fernando-herrera.com-angular-adv/03-adminpro/src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-rxjs',
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
   ]
 })
 export class RxjsComponent {
+  constructor() {
+    const obs$ = new Observable(observer => {
+      let i= -1;
 
+      const intervalo = setInterval(() => {
+        i++;
+        observer.next(i);
+        if ( i === 4){
+          clearInterval(intervalo);
+          observer.complete();
+        }
+        if(i === 2 ){
+          observer.error('I llego al valor de 2');
+        }
+      },1000)
+    });
+
+    obs$.subscribe(
+      valor => console.log('subs:',valor),
+      (err) => console.warn('Error',err),
+      () => console.info('obs terminado')
+    );
+  }
 }
