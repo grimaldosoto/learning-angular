03470764028c455861d8b7385bca24c604e367ec
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-09-02 10:39:37 -0500

    Operador map de los observables

--- udemy-fernando-herrera.com-angular-adv/03-adminpro/src/app/pages/rxjs/rxjs.component.ts
+++ udemy-fernando-herrera.com-angular-adv/03-adminpro/src/app/pages/rxjs/rxjs.component.ts
@@ -1,39 +1,49 @@
-import { Component } from '@angular/core';
-import {Observable, retry} from "rxjs";
+import {Component} from '@angular/core';
+import {interval, map, Observable, retry, take} from "rxjs";
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class RxjsComponent {
   constructor() {
-
-    this.retornaObservable().pipe(
-     retry(1)
-    ).subscribe(
-      valor => console.log('subs:',valor),
-      (err) => console.warn('Error',err),
-      () => console.info('obs terminado')
-    );
+    //
+    // this.retornaObservable().pipe(
+    //  retry(1)
+    // ).subscribe(
+    //   valor => console.log('subs:',valor),
+    //   (err) => console.warn('Error',err),
+    //   () => console.info('obs terminado')
+    // );
+    this.returnaIntervalo().subscribe(
+      console.log
+    )
   }
 
-  retornaObservable() : Observable<number>{
-    let i= -1;
+  returnaIntervalo(): Observable<number> {
+    return interval(1000)
+      .pipe(
+        take(4),
+        map(valor => valor + 1)
+      );
+  }
+
+  retornaObservable(): Observable<number> {
+    let i = -1;
 
     return new Observable<number>(observer => {
       const intervalo = setInterval(() => {
         i++;
         observer.next(i);
-        if ( i === 4){
+        if (i === 4) {
           clearInterval(intervalo);
           observer.complete();
         }
-        if(i === 2 ){
+        if (i === 2) {
           observer.error('I llego al valor de 2');
         }
-      },1000)
+      }, 1000)
     });
 
 
