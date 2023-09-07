ef0fabc0136acf9e5141bd5d28996ac356d94e3d
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-09-06 20:04:14 -0500

    Primera ruta

--- udemy-fernando-herrera.com-angular-adv/04-backend-server/index.js
+++ udemy-fernando-herrera.com-angular-adv/04-backend-server/index.js
@@ -3,7 +3,14 @@ const express = require('express');
 //crear el servidor de express
 const app = express();
 
+//rutas
+app.get('/',(req, res)=>{
+    res.json({
+        ok: true,
+        msg: 'Hola Mundo'
+    });
+});
 
 app.listen(3000, ()=>{
     console.log('servidor corriendo en puerto:' + 3000);
-})
+});
