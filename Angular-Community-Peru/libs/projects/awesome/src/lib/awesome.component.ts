a3032e3f87f9e79e1de009cb9dd2f2903c0a5ecc
Grimaldo SOTO <grimaldo.soto@outlook.com>
2024-05-02 14:17:56 -0500

    Creación de nueva app mfe, host y libs

---
+++ Angular-Community-Peru/libs/projects/awesome/src/lib/awesome.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-awesome',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      awesome works!
+    </p>
+  `,
+  styles: ``
+})
+export class AwesomeComponent {
+
+}
---
+++ Angular-Community-Peru/libs/projects/awesome/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of awesome
+ */
+
+export * from './lib/awesome.service';
+export * from './lib/awesome.component';
