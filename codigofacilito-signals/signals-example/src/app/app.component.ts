465e26d0acca42e4fbd1f36bd2d99794eac17994
Grimaldo SOTO <grimaldo.soto@outlook.com>
2024-05-16 11:49:54 -0500

    Signals API Update y Mutate

--- codigofacilito-signals/signals-example/src/app/app.component.ts
+++ codigofacilito-signals/signals-example/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import {Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgForOf} from "@angular/common";
+
+interface Task{
+  name: string;
+  isCompleted: boolean;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -12,8 +18,38 @@ export class AppComponent {
   title = 'signals-example';
 
   name = signal('Grimaldo');
+  description = signal('LOREM').asReadonly();
+  tasks = signal<Task[]>([{
+    name: 'Ver Curso Signals', isCompleted: false
+  }]);
 
   toggleName(){
     this.name.set('Soto');
+  }
+
+  addRandomTask(){
+    this.tasks.update((tasks) => {
+      return [...tasks,{name: 'Practicar mucho', isCompleted: false}];
+    });
+  }
+
+  markTaskAsComplete(){
+    /** In Angular 17 the method mutate has been removed **/
+//    this.tasks.mutate(task => {
+//      const taskToUpdate = this.tasks()[0];
+//      if(taskToUpdate){
+//        task.name = "Practicar mucho Signals";
+//        taskToUpdate.isCompleted = true;
+//        }
+//    });
+    this.tasks.update((oldTasks: Task[]) => {
+      const newTask = [...oldTasks];
+      const taskToUpdate = newTask[0];
+      if(taskToUpdate){
+        taskToUpdate.name = "Practicar mucho Signals!!";
+        taskToUpdate.isCompleted = true;
+      }
+      return newTask;
+    });
   }
 }
