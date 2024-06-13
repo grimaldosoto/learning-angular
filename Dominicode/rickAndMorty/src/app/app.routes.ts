94e966be713fd119f9f82730a9954bcbe488a2f6
Grimaldo SOTO <grimaldo.soto@outlook.com>
2024-06-12 20:10:37 -0500

    creación de estructuras con componentes

--- Dominicode/rickAndMorty/src/app/app.routes.ts
+++ Dominicode/rickAndMorty/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./views/pages/pages.module').then(m => m.PagesModule),
+  },
+  {
+    path: 'character-list',
+    loadChildren: () =>
+      import('./views/pages/characters/characters.module').then(m => m.CharactersModule),
+  },
+  {
+    path: 'character-details/:id',
+    loadChildren: () =>
+      import('./views/pages/characters/character-details/character-details.module').then(m => m.CharacterDetailsModule),
+  }
+];
