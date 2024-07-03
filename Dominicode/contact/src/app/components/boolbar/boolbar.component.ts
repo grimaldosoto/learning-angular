58bfc51cd1e218a70b227f95509e310cdd082e30
Grimaldo SOTO <grimaldo.soto@outlook.com>
2024-07-03 16:01:06 -0500

    add: components
    creación de componentes y configuración de firebase en el envs

---
+++ Dominicode/contact/src/app/components/boolbar/boolbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boolbar',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      boolbar works!
+    </p>
+  `,
+  styles: ``
+})
+export class BoolbarComponent {
+
+}
---
+++ Dominicode/contact/src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  firebase:{"projectId":"crud-angular-18-9e397","appId":"1:969078763351:web:ae02b35f4d3a76f01cd812","storageBucket":"crud-angular-18-9e397.appspot.com","apiKey":"AIzaSyD5RPzL5hY0wF17n3XKgnGHbj8LGcM9wo4","authDomain":"crud-angular-18-9e397.firebaseapp.com","messagingSenderId":"969078763351","measurementId":"G-33ZEWMMZRW"}
+};
