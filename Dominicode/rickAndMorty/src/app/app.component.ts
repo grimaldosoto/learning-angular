461c40329883d0d6ee8ba7d99a42727d6ddbb7bf
Grimaldo SOTO <grimaldo.soto@outlook.com>
2024-07-17 16:06:49 -0500

    add: header template

--- Dominicode/rickAndMorty/src/app/app.component.ts
+++ Dominicode/rickAndMorty/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HomeComponent} from "./views/pages/home/home.component";
+import { HeaderComponent } from "./views/layout/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
