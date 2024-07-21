1cb73076f7ff43f5f3eec533b628d4eaf01ed647
Grimaldo SOTO <grimaldo.soto@outlook.com>
2024-07-20 23:01:28 -0500

    agregado services characters y la interfaces
    se agregó el servicio de characters con 2 metodos su respectivo interface character

---
+++ Dominicode/rickAndMorty/src/app/shared/services/character.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Character} from "../../views/layout/shared/interfaces/character.interface";
+import {environment} from "../../../environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CharacterService {
+
+  constructor(private http: HttpClient) { }
+
+  searchCharacters(query = '', page = 1){
+    const filter = `${environment.baseUrlAPI}/?name=${query}&page=${page}`;
+    return this.http.get<Character[]>(
+      filter
+    );
+  }
+
+  getDetails(id: number){
+    return this.http.get<Character[]>(
+      `${environment.baseUrlAPI}/id=${id}`
+    )
+  }
+}
